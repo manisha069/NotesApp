91124b4b46cd389795559528d3175fb70fa4b874
Manisha <manisha.guptacs2018@indoreinstitute.com>
2025-01-03 22:46:49 +0530

    resolved delete issue, fixed input box outline

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -12,10 +12,13 @@ import { CommonModule, DatePipe } from '@angular/common';
 })
 export class NotesComponent {
 
-  notesList:any =[];
+  // notesList:any =[];
   newNote:any;
   today:any;
   emptyNote:boolean =false;
+  toDeleteIndex :any;
+  idCounter:number = 0;
+ dataList : any=[];
 
   inputFunction(){
   this.emptyNote = false;
@@ -26,23 +29,42 @@ export class NotesComponent {
   this.emptyNote = false;
 
     if(newN != "" && newN != null){
-      this.notesList.push(newN);
-      console.log("noteslist", this.notesList);
+      // this.notesList.push(newN);
+      // console.log("noteslist", this.notesList);
       this.newNote = "";
       this.today = Date.now();
-
+      this.dataList.push([this.idCounter, newN]);
+      this.idCounter = this.idCounter + 1;
+      console.log(this.dataList);
     }
     else{
       this.emptyNote = true;
     }
   }
 
-  deleteNote(doc:any){
-    console.log("in delete", doc);
-    this.notesList.forEach((item:any, index:any)=>{
-      if (item === doc)
-        this.notesList.splice(index, 1);
-    });
+  deleteNote(){
+    console.log("in delete", this.toDeleteIndex);
+    console.log("len", this.dataList.length);
+
+    for(let i=0;i< this.dataList.length; i++){
+      console.log("index at i", i);
+      if((this.dataList[i][0]) === this.toDeleteIndex){
+    console.log("in if condition", this.dataList[i], this.dataList[i][0], this.dataList[i][1]);
+
+        this.dataList.splice(i, 1);
+        console.log("updated", this.dataList);
+        this.closePopup();
+        break;
+      }
+      
+    }
+    // this.dataList.forEach((item:any)=>{
+    //   console.log("item", item);
+    //   if (item[0] === this.toDeleteIndex)
+    //     this.dataList.splice(this.toDeleteIndex, this.toDeleteIndex+1);
+    //     break;
+    //     this.closePopup();
+    // });
 
     // let foo_object; // Itemitem(object here) to remove
 // this.foo_objects = this.foo_objects.filter(obj => return obj !== foo_object);
@@ -54,7 +76,9 @@ export class NotesComponent {
     
   }
 
-  deletePopup(){
+  deletePopup(num:any){
+    this.toDeleteIndex = num;
+    console.log("pop up num", num)
     let modal = document.getElementById("DeletePopup")!;
     modal.style.display= "block";
   }
